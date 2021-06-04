c77512c7e53a38c4a29bdd37d85ef4f48ac82cdc
VihaanS22 <69805124+VihaanS22@users.noreply.github.com>
2021-06-04 20:37:54 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react'; 
-import { StyleSheet, Text, View } from 'react-native'; 
-import WelcomeScreen from './screens/WelcomeScreen'; 
-
-export default function App() { 
   
-  return ( 
-  <WelcomeScreen/> 
-  ); 
+import React from 'react';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import { AppDrawerNavigator } from './components/AppDrawerNavigator'
+
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
 }
+
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen:{screen: WelcomeScreen},
+  Drawer:{screen: AppDrawerNavigator}
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
--- components/SantaClaus.js
+++ components/SantaClaus.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 import LottieView from "lottie-react-native";
-export default class AppHeader extends React.Component {
+export default class BasicExample extends React.Component {
   render() {
     return (
+     /*
       <LottieView
       ref={animation => {
         this.animation = animation;
@@ -12,12 +13,13 @@ export default class AppHeader extends React.Component {
       style={{
         width: 400,
         height: 400,
-        backgroundColor: '#eee',
+        backgroundColor: 'white',
       }}
-      source={require('./assets/13015-santa-claus.json')}
-      // OR find more Lottie files @ https://lottiefiles.com/featured
-      // Just click the one you like, place that file in the 'assets' folder to the left, and replace the above 'require' statement
+      
+   //   source={require('../assets/13015-santa-claus.json')} autoPlay loop 
     />
+    */
+   <Text>Hello</Text>
     );
   }
 }
